bce9f27cda7c0923af716d1dedcab469c24d827d
Vladislav-Ch <44359375+Vladislav-Ch@users.noreply.github.com>
2019-04-27 16:41:34 +0300

    Delete

--- IT_Джин/js/js.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function() {
-    jQuery('.toggle-nav').click(function(e) {
-        jQuery(this).toggleClass('active');
-        jQuery('.top-header-menu ul').toggleClass('active');
-
-        e.preventDefault();
-    });
-});
